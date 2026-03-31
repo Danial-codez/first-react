a3702cf1120627deb250f70637d5a8b5fe1d5319
Danial-codez <danialzia2004@gmail.com>
2026-03-31 15:33:54 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className="card">
+        <h3>Kya naam hai bhai</h3>
+        <p>kya ker rahe ho???</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
